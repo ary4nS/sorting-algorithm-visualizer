5a2afa39833c3d81afe8217b5b0cabbf9e857d2f
erulli <117382591+onion-milkshake@users.noreply.github.com>
2023-09-02 00:29:00 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const random = [];
 
         for (let i = 0; i < N; i++) {
-            const number = Math.floor(Math.random() * N);
+            const number = Math.floor(Math.random() * 65);
             random.push(number);
         }
 
@@ -23,6 +23,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // Handling the input event 
     slider.addEventListener(
         'input', () => {
+
+            // clearing the inner html contents of barContainer
+            barContainer.innerHTML = '';
+
             let sliderValue = slider.value;
             const arr = randomNumberGenerator(sliderValue);
 
@@ -35,10 +39,21 @@ document.addEventListener("DOMContentLoaded", function () {
                     const side = document.createElement('div');
                     side.className = `side ${sideName}`;
 
-                    if (['back', 'right', 'front'].includes(sideName)) {
+                    if (['back', 'right', 'front', 'left'].includes(sideName)) {
+                        const colorBar = document.createElement('div');
+                        colorBar.className = 'color-bar';
 
+                        // Setting the height of the color bar
+                        colorBar.style.height = `${value}vh`;
+                        colorBar.style.transform = `translateY(${70 - value}vh)`;
+
+                        side.appendChild(colorBar);
                     }
+
+                    bar.appendChild(side);
                 });
+
+                barContainer.appendChild(bar);
             });
         }
     );
