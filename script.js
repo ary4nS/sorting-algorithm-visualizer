a36c68c8527db4aef24ab7c34028fd1da38cbda8
erulli <117382591+onion-milkshake@users.noreply.github.com>
2023-09-02 10:57:36 +0530

    ADDED BUTTONS

--- script.js
+++ script.js
@@ -20,6 +20,36 @@ document.addEventListener("DOMContentLoaded", function () {
     // Elements related to the bar container
     const barContainer = document.querySelector('.bar-ctn');
 
+    // Global slider value 
+    let sliderValue = 3;
+
+    // When the project loads, three bars must be displayed by default 
+    const arr = randomNumberGenerator(3);
+
+    arr.forEach((value, i) => {
+        const bar = document.createElement('div');
+        bar.className = 'bar';
+
+        const sides = ['top', 'bottom', 'right', 'left', 'front', 'back'];
+        sides.forEach((sideName) => {
+            const side = document.createElement('div');
+            side.className = `side ${sideName}`;
+
+            if (['back', 'right', 'front', 'left'].includes(sideName)) {
+                const colorBar = document.createElement('div');
+                colorBar.className = 'color-bar';
+
+                // Setting the height of the color bar
+                colorBar.style.height = `${value}vh`;
+                colorBar.style.transform = `translateY(${70 - value}vh)`;
+
+                side.appendChild(colorBar);
+            }
+            bar.appendChild(side);
+        });
+        barContainer.appendChild(bar);
+    });
+
     // Handling the input event 
     slider.addEventListener(
         'input', () => {
@@ -27,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // clearing the inner html contents of barContainer
             barContainer.innerHTML = '';
 
-            let sliderValue = slider.value;
+            sliderValue = slider.value;
             const arr = randomNumberGenerator(sliderValue);
 
             arr.forEach((value, i) => {
@@ -49,12 +79,49 @@ document.addEventListener("DOMContentLoaded", function () {
 
                         side.appendChild(colorBar);
                     }
-
                     bar.appendChild(side);
                 });
-
                 barContainer.appendChild(bar);
             });
         }
     );
-});
+
+    // Elements related to buttons 
+    const generateNewArrayBtn = document.querySelector('.gen-btn')
+
+
+    // Elements related to the bars
+    const bars = document.querySelectorAll('.bar');
+    const sides = document.querySelectorAll('.side');
+    console.log(bars);
+    console.log(sides);
+
+
+    // Handling the generate new array button 
+    generateNewArrayBtn.addEventListener(
+        'click', () => {
+            generateNewArrayBtn.classList.add('clickable');
+
+            sliderValue = slider.value;
+            const arr = randomNumberGenerator(sliderValue);
+
+            let index = 0;
+
+            bars.forEach((bar) => {
+                sides.forEach((side) => {
+                    if (['back', 'right', 'front', 'left'].includes(side.classList)) {
+                        let value = arr[index++];
+                        console.log(value);
+                        const colorBar = side.querySelector('.color-bar');
+                        colorBar.style.height = `${value}vh`;
+                        colorBar.style.transform = `translateY(${70 - value}vh)`;
+                    }
+                });
+            });
+
+            setTimeout(() => {
+                generateNewArrayBtn.classList.remove('clickable');
+            }, 100);
+        }
+    );
+}); 
